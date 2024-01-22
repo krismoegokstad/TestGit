d5deaaa7e6e32621ea40be6034958f74b66ca451
krismoegokstad <kristian.moe@gokstadakademiet.no>
2024-01-22 08:50:44 +0100

    added a function to count to 100

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace TestGit;
-
-class Program
+﻿namespace TestGit
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Kristian, Moe!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kristian, Moe!");
+            
+            CountTo100();
+        }
+        
+        static void CountTo100()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
