29621387ff57f2602ffd05505ff5294f82c3b067
krismoegokstad <kristian.moe@gokstadakademiet.no>
2024-01-22 08:51:23 +0100

    added comments to the function to count to 100

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,9 +6,11 @@
         {
             Console.WriteLine("Kristian, Moe!");
             
+            // Call the new CountTo100 function
             CountTo100();
         }
-        
+
+        // Function to count to 100
         static void CountTo100()
         {
             for (int i = 1; i <= 100; i++)
